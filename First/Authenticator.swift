73ae01f42c21b0b7ea9230fa74e176f54e688a0f
Mayank Yadav <mayank@spotify.com>
2015-08-30 17:34:17 +0200

    Remove unnecessary methods

--- First/Authenticator.swift
+++ First/Authenticator.swift
@@ -17,34 +17,10 @@ class Authenticator {
         container.accountStatusWithCompletionHandler { (status, error) -> Void in
 
             if status == CKAccountStatus.NoAccount {
-                showSignIntoICloudAlert({ (complete) -> Void in
-                    completionHandler(false)
-                })
+                completionHandler(false)
             } else {
                 completionHandler(true)
             }
         }
-    }
-
-    class private func showSignIntoICloudAlert(completion:(Bool)->Void) {
-
-        let actionTitle = NSLocalizedString("Okay", comment: "Okay")
-        let alertController = signInICloudAlert()
-        let alertAction = UIAlertAction(title: actionTitle, style: UIAlertActionStyle.Cancel) { (alertAction) -> Void in
-            completion(true)
-        }
-
-        alertController.addAction(alertAction)
-
-        let rootViewController = UIApplication.sharedApplication().keyWindow?.rootViewController
-        rootViewController?.presentViewController(signInICloudAlert(), animated: true, completion: nil)
-    }
-
-    class private func signInICloudAlert() -> UIAlertController {
-
-        let title = NSLocalizedString("Sign in to iCloud", comment: "Sign in to iCloud")
-        let message = NSLocalizedString("Sign in to your iCloud account to write records. On the Home screen, launch Settings, tap iCloud, and enter your Apple ID. Turn iCloud Drive on. If you don't have an iCloud account, tap Create a new Apple ID.", comment: "iCloud Signin alert message")
-        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
-        return alert
     }
 }
